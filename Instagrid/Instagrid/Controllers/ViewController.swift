fd0fd6b232376f1fa98f886606ab210396d2f40b
MadNoodle <mjanneau@gmail.com>
2017-11-17 17:08:21 +0100

    first project setup ( icons, assets, fonts)

---
+++ Instagrid/Instagrid/Controllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagrid
+//
+//  Created by Mathieu Janneau on 17/11/2017.
+//  Copyright © 2017 Mathieu Janneau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+

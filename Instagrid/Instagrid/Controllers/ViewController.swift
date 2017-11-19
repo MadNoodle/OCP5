0d2f62f561fd4214da423b967b483b54cf56d4fe
MadNoodle <mjanneau@gmail.com>
2017-11-19 21:41:17 +0100

    Mise en place des layouts et correction des contraintes internes à layoutView

--- Instagrid/Instagrid/Controllers/ViewController.swift
+++ Instagrid/Instagrid/Controllers/ViewController.swift
@@ -10,14 +10,76 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  // MARK: VARIABLE DECLARATIONS
   
+  // Buttons declarations
+  @IBOutlet weak var layoutTwoButton: UIButton!
+  @IBOutlet weak var layoutThreeButton: UIButton!
+  @IBOutlet weak var layoutOneButton: UIButton!
+  // Buttons Hover Déclarations
+  @IBOutlet weak var buttonTwoHover: UIImageView!
+  @IBOutlet weak var buttonOneHover: UIImageView!
+  @IBOutlet weak var buttonThreeHover: UIImageView!
   
+  // Layout Elements Delcarations
+  @IBOutlet weak var squareOne: UIView!
+  @IBOutlet weak var squareTwo: UIView!
+  @IBOutlet weak var squareThree: UIView!
+  @IBOutlet weak var squareFour: UIView!
+  @IBOutlet weak var rectBot: UIView!
+  @IBOutlet weak var rectTop: UIView!
+  
+  // MARK: CORE UI VIEW FUNCTIONS
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    showLayoutOne()
   }
 
+  // MARK: BUTTONS INTERACTIONS
+  @IBAction func selectlayoutOne() {
+    showLayoutOne()
+    buttonOneHover.isHidden = false
+    buttonTwoHover.isHidden = true
+    buttonThreeHover.isHidden = true
+  }
+  
+  @IBAction func selectLayoutTwo() {
+    showLayoutTwo()
+    buttonOneHover.isHidden = true
+    buttonTwoHover.isHidden = false
+    buttonThreeHover.isHidden = true
+  }
+  
+  @IBAction func selectLayoutThree() {
+    showLayoutThree()
+    buttonOneHover.isHidden = true
+    buttonTwoHover.isHidden = true
+    buttonThreeHover.isHidden = false
+  }
+  private func showLayoutOne(){
+    rectTop.isHidden = false
+    rectBot.isHidden = true
+    squareOne.isHidden = true
+    squareTwo.isHidden = true
+    squareThree.isHidden = false
+    squareFour.isHidden = false
+  }
 
-
+  private func showLayoutTwo(){
+    rectTop.isHidden = true
+    rectBot.isHidden = false
+    squareOne.isHidden = false
+    squareTwo.isHidden = false
+    squareThree.isHidden = true
+    squareFour.isHidden = true
+  }
+  private func showLayoutThree(){
+    rectTop.isHidden = true
+    rectBot.isHidden = true
+    squareOne.isHidden = false
+    squareTwo.isHidden = false
+    squareThree.isHidden = false
+    squareFour.isHidden = false
+  }
 }
 

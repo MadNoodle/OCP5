62d4505f9473b34fc24ec9cd075e225838d5d18b
MadNoodle <mjanneau@gmail.com>
2017-11-29 12:30:28 +0100

    navbar for collectionView added and search functionnality added

--- Instagrid/Instagrid/Controllers/CollectionViewController.swift
+++ Instagrid/Instagrid/Controllers/CollectionViewController.swift
@@ -12,6 +12,7 @@ import UIKit
 /**
  CollectionView Controller that handles the display of the photo search on Pixabay
  */
+@IBDesignable
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
   
   let Api = APIClient()
@@ -20,25 +21,12 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupNavbar()
     self.collectionView?.delegate = self
-    collectionView?.backgroundColor = UIColor.white
+    collectionView?.backgroundColor = UIColor(red: 186/255.0, green: 215/255.0, blue: 230/255.0, alpha: 1.0)
     collectionView?.register(ImageViewCell.self, forCellWithReuseIdentifier: "cellId")
-    
-    
-    self.showSpinner()
-    
-    
-    Api.getImagesAPI(query: "cat") {(results:[UIImage]) in
-      
-      for result in results {
-        self.imageResults.append(result)
-        DispatchQueue.main.async {
-          self.collectionView?.reloadData()
-          self.hideSpinner()
-        }
-      }
     }
-    }
+  
   func showSpinner(){
     
     
@@ -59,27 +47,88 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
   
   func hideSpinner(){
     myActivityIndicator.removeFromSuperview()
-    
   }
+  
+  
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
     return imageResults.count
   }
+  
+  
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> ImageViewCell {
     
     let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath) as! ImageViewCell
     print("RECU2: \(imageResults)")
     cell.displayContent(image: (imageResults[indexPath.row]))
-    
     return cell
     
   }
-  // defin e the layout and size of a cell
+  // define the layout and size of a cell
   func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath:IndexPath) -> CGSize {
     return CGSize(width: view.frame.width, height: 200)
     
   }
   
 
+/// Setup Navbar
+  func setupNavbar(){
+    
+    let margin : CGFloat = 8
+    let buttonWidth : CGFloat = 100
+    let buttonHeight : CGFloat = 50
+    
+    let navBar: UIView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: 60))
+    navBar.backgroundColor = UIColor.white
+    self.view.addSubview(navBar)
+    let navBarWidth : CGFloat = navBar.frame.width
+    
+    //add cancel button
+    let cancel = UIButton(frame: CGRect(x: margin, y: margin, width: buttonWidth, height: buttonHeight))
+    cancel.setTitle("Cancel", for: .normal)
+    cancel.setTitleColor(UIColor(red: 16/255.0, green: 102/255.0, blue: 152/255.0, alpha: 1.0), for: .normal)
+    cancel.addTarget(self, action: #selector(self.goBack), for: .touchUpInside)
+    navBar.addSubview(cancel)
+    
+    //add Search button
+    let search = UIButton(frame: CGRect(x: navBarWidth - (margin + buttonWidth), y: margin, width: buttonWidth, height: buttonHeight))
+    search.setTitle("Search", for: .normal)
+    search.setTitleColor(UIColor(red: 16/255.0, green: 102/255.0, blue: 152/255.0, alpha: 1.0), for: .normal)
+    search.addTarget(self, action: #selector(self.performSearch), for: .touchUpInside)
+    navBar.addSubview(search)
+    
+    //add textInput for search
+    let searchInput = UITextField(frame: CGRect(x: (2 * margin + buttonWidth ), y: margin, width: navBarWidth - (2 * margin + buttonWidth ) - (2 * margin + buttonWidth ), height: buttonHeight))
+    searchInput.placeholder = "Enter your request"
+    searchInput.tag = 10
+    navBar.addSubview(searchInput)
+  }
+  
 
+  @objc func goBack(sender : UIButton){
+    dismiss(animated: true, completion: nil)
+  }
+  
+  @objc func performSearch(sender : UIButton){
+    
+    let textfield = view.viewWithTag(10) as? UITextField
+    
+      if let query = textfield?.text {
+      self.imageResults = []
+      self.showSpinner()
+      Api.getImagesAPI(query: query) {(results:[UIImage]) in
+        
+        for result in results {
+          
+          self.imageResults.append(result)
+          DispatchQueue.main.async {
+            self.collectionView?.reloadData()
+            self.hideSpinner()
+          }
+        }
+      }
+      
+      
+    }
+  }
   
 }
--- Instagrid/Instagrid/Views/ImageViewCell.swift
+++ Instagrid/Instagrid/Views/ImageViewCell.swift
@@ -21,6 +21,7 @@ class ImageViewCell: UICollectionViewCell {
     super.init(frame:frame)
     
     setupImage()
+    
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -40,6 +41,14 @@ class ImageViewCell: UICollectionViewCell {
     return imageView
   }()
   
+  var Button : UIButton = {
+    let useButton = UIButton(frame: CGRect(x: 8, y: 200 - 8 , width: 50, height: 50))
+  useButton.setTitle("USE IMAGE", for: .normal)
+  useButton.setTitleColor(UIColor(red: 16/255.0, green: 102/255.0, blue: 152/255.0, alpha: 1.0), for: .normal)
+  useButton.addTarget(self, action: #selector(useImage), for: .touchUpInside)
+    return useButton
+  }()
+  
   // MARK: METHODS
   /**
    This function create a subView of type UIImageView and applies constraints to it
@@ -47,17 +56,21 @@ class ImageViewCell: UICollectionViewCell {
   func setupImage(){
     // adding UIImage View in Cell
     addSubview(previewImage)
+    addSubview(Button)
     
     
     //Horizontal Constraints
     addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-16-[v0]-16-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":previewImage]))
     
     // VErtical Constraints
-    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-4-[v0]-4-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":previewImage]))
+    addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[v0]-0-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":previewImage]))
    
     
   }
   
+
+  
+  @objc func useImage(sender: UIButton){}
   func displayContent(image: UIImage) {
     previewImage.image = image
     

aafe79f2a65082c424180e7d54cee51cbb22a8bd
MadNoodle <mjanneau@gmail.com>
2017-12-02 20:08:53 +0100

    add emoji assets

--- Instagrid/Instagrid/Controllers/EditImageController.swift
+++ Instagrid/Instagrid/Controllers/EditImageController.swift
@@ -16,10 +16,29 @@ class EditImageController: UIViewController {
   //Var to store the image from HomeVC
   var tag : UIImageView?
   var finalImage = UIImage()
+  var panGesture = UIPanGestureRecognizer()
+  var pinch = UIPinchGestureRecognizer()
+   let emojiVc = EmojiCollectionViewController(nibName: "EmojiCollectionViewController", bundle: nil) 
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    panGesture = UIPanGestureRecognizer(target: self, action: #selector(viewDragged))
+    pinch = UIPinchGestureRecognizer(target: self, action: #selector(scale))
+    
+    self.view.addGestureRecognizer(pinch)
+    self.textInput.addGestureRecognizer(panGesture)
+    
+    self.textInput.isUserInteractionEnabled = true
+    self.view.isMultipleTouchEnabled = true
+    
+    
+  
   }
+  
+
+  
+  
   //Load image after all the views had been initialize
   override func viewWillAppear(_ animated: Bool) {
     getImage()
@@ -46,25 +65,65 @@ class EditImageController: UIViewController {
   }
   
   
-  @IBAction func filter(_ sender: Any) {
-    fxContainer.isHidden = false
-  }
   
-  @IBAction func insertText(_ sender: Any) {
-    container.addSubview(textInput)
-    
-  }
+  
   
   
   @IBAction func insertImage(_ sender: Any) {
+    
+    emojiVc.modalPresentationStyle = .overFullScreen
+    present(emojiVc, animated: true,completion:nil)
   }
+  
+  // ///////////////////////////// //
+  // MARK: GESTURE CALLBACKS      //
+  // ///////////////////////////// //
   
   
   @IBOutlet weak var container: UIView!
   
+  @objc func viewDragged(){
+    
+    let newOrigin = panGesture.location(in: self.container)
+    if newOrigin.x >= textInput.frame.width / 2 && newOrigin.y >= textInput.frame.height / 2 && newOrigin.x <= container.frame.width - textInput.frame.width / 2 && newOrigin.y <= container.frame.height - textInput.frame.height / 2{
+      let newFrame = CGRect(x: newOrigin.x - (textInput.frame.width / 2), y: newOrigin.y - (textInput.frame.height / 2), width: textInput.frame.width, height: textInput.frame.height)
+      textInput.frame = newFrame
+      
+    }
+    else {print("Out of bounds")}
+    
+    print(newOrigin)
+    print(container.frame.width)
+    print(container.frame.height)
+  }
+  
+  
+  @objc func scale(sender:UIPinchGestureRecognizer){
+    print(pinch.scale)
+    var pointSize = textInput.font?.pointSize
+    /// - Todo: set min and max font
+    /// - Todo: clip to view bounds
+    pointSize = ((sender.velocity > 0) ? 1 : -1) * 1 + pointSize!;
+    textInput.font = UIFont( name: (textInput.font?.fontName)!, size: (pointSize)!)
+    
+    let newFrame = CGRect(x: textInput.frame.origin.x , y: textInput.frame.origin.y, width: textInput.frame.width * pinch.scale, height: textInput.frame.height * pinch.scale)
+    
+    textInput.sizeToFit()
+    textInput.frame = newFrame
+    textInput.setNeedsDisplay()
+    pinch.scale = 1
+    
+  }
+  
   // ///////////////////////////// //
   // MARK: APPLYING FILTERS       //
   // ///////////////////////////// //
+  
+  @IBAction func filter(_ sender: Any) {
+    fxContainer.isHidden = false
+    fontContainer.isHidden = true
+  }
+  
   
   @IBAction func applyInstantFilter(_ sender: Any) {
     fxEditor.applyFilter("CIPhotoEffectInstant",on: imageToEdit)
@@ -86,22 +145,75 @@ class EditImageController: UIViewController {
     
   }
   
+  // ///////////////////////////// //
+  // MARK: TYPING AND EDITING TEXT     //
+  // ///////////////////////////// //
+  
+  @IBOutlet weak var fontContainer: UIView!
+  @IBAction func insertText(_ sender: Any) {
+    container.addSubview(textInput)
+    fontContainer.isHidden = false
+    fxContainer.isHidden = true
+    
+  }
   
   var textInput: UITextField = {
-    
-    let textInput = UITextField(frame: CGRect(x: 100, y: 100, width: 300, height: 50))
+    let textInput = UITextField(frame: CGRect(x: 100, y: 100, width: 500, height: 100))
     textInput.isUserInteractionEnabled = true
+    
     textInput.placeholder = "Tap here to enter your text"
     textInput.textAlignment = .center
     textInput.placeHolderColor = UIColor.white
     
-    textInput.font = UIFont(name:"delm-medium", size: 20)
+    textInput.font = UIFont(name:"delm-medium", size: 14)
     textInput.textColor = UIColor.white
     textInput.sizeToFit()
     return textInput
   }()
   
   
+  // MARK : CHANGING FONTS TYPE
+  @IBAction func switchfont1(_ sender: Any) {
+    textInput.font = UIFont(name:"delm-medium", size: 14)
+  }
+  
+  @IBAction func switchfont2(_ sender: Any) {
+    textInput.font = UIFont(name:"ThirstySoftRegular", size: 10)
+  }
+  
+  @IBAction func switchFont3(_ sender: Any) {
+    textInput.font = UIFont(name:"Didot", size: 10)
+  }
+  
+  // MARK : CHANGING FONTS COLOR
+  @IBAction func yellow(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.yellow
+    textInput.textColor = UIColor.yellow
+  }
+  
+  @IBAction func blue(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.blue
+    textInput.textColor = UIColor.blue
+  }
+  
+  @IBAction func green(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.green
+    textInput.textColor = UIColor.green
+  }
+  
+  @IBAction func pink(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.purple
+    textInput.textColor = UIColor.purple
+  }
+  @IBAction func white(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.white
+    textInput.textColor = UIColor.white
+  }
+  
+  @IBAction func black(_ sender: Any) {
+    textInput.placeHolderColor = UIColor.black
+    textInput.textColor = UIColor.black
+  }
 }
 
 extension UITextField{
@@ -112,5 +224,9 @@ extension UITextField{
     set {
       self.attributedPlaceholder = NSAttributedString(string:self.placeholder != nil ? self.placeholder! : "", attributes:[NSAttributedStringKey.foregroundColor: newValue!])
     }
+  }
+  func scaleFontSize (scale: CGFloat) {
+    
+    self.font =  UIFont(name: (self.font?.fontName)!, size: (self.font?.pointSize)! * scale)!
   }
 }
---
+++ Instagrid/Instagrid/Controllers/EmojiViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  EmojiViewCell.swift
+//  Instagrid
+//
+//  Created by Mathieu Janneau on 02/12/2017.
+//  Copyright © 2017 Mathieu Janneau. All rights reserved.
+//
+
+import UIKit
+
+class EmojiViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var emojiImage: UIImageView!
+}
